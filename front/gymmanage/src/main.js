71d732285ef21fb861b38342e18a09e6a3843558
= <=>
2024-03-12 23:46:34 +0800

    配置element-plus语言

--- front/gymmanage/src/main.js
+++ front/gymmanage/src/main.js
@@ -10,8 +10,17 @@ import 'element-plus/dist/index.css'
 // import * as Icons from '@element-plus/icons'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// 引入中文包
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).use(VueAxios,axios).use(store).use(ElementPlus).use(router).mount('#app')
+createApp(App)
+  .use(VueAxios,axios)
+  .use(store)
+  .use(ElementPlus, {
+    locale: zhCn
+  })
+  .use(router)
+  .mount('#app')
 
 // Object.keys(Icons).forEach(key => {
 //     App.component(key, Icons[key])
